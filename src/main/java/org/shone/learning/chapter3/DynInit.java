dd921212fd4a4e3238fe16b144f6f48c3fe60cb6
shone <sultanov.dinnur@gmail.com>
2017-06-17 15:53:37 +0300

    commit

--- src/main/java/org/shone/learning/chapter3/DynInit.java
+++ src/main/java/org/shone/learning/chapter3/DynInit.java
@@ -5,6 +5,6 @@ public class DynInit {
         double a = 3.0, b = 4.0;
 
         double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-        System.out.println(c);
+        System.out.println("hypotenuse = " + c);
     }
 }
